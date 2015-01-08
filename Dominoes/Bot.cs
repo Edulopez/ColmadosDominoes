fbf390a905f149c5af985efd2cd2c95bb556d524
unknown <elopez@DEV12.siuben.gob.do>
2015-01-08 14:38:18 -0400

    First Commit
    First version of the project

---
+++ Dominoes/Bot.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominoes
+{
+    class Bot : Player
+    {
+        const int DUMMYVAL = 1000000;
+        int RecursiveCalls = 11;
+        public Hashtable dpHashTable;
+
+        public Bot(List<Tuple<int, int>> StartHand, int TypeOfPlayer, int _idPlayer): base(StartHand, TypeOfPlayer, _idPlayer)
+        {
+            dpHashTable = new Hashtable();
+            AvailableDominoes.Sort();
+            Hand.Sort();
+        }
+
+        // retorna los puntos del juego.
+        private int Eval(LinkedList<Tuple<int, int>> ActualGame)
+        {
+            int pointsInHand = 1;
+            for (int i = 0; i < Hand.Count; ++i)
+                pointsInHand += Hand[i].Item1 + Hand[i].Item2;
+
+            for (int i = 0; i < EnemysHandCount.Length; ++i)
+                if (i != idPlayer)
+                {
+                    if(EnemysHandCount[i]==0) return DUMMYVAL;
+                } 
+
+            return pointsInHand;
+        }
+
+        public Tuple<int, int> MakeAMove(LinkedList<Tuple<int, int>> ActualGame)
+        {
+            Tuple<int, int> UsingDomino=null;
+            if (!CanPlay(ActualGame)) return null;
+            else
+            {
+                int stop = Hand.Count;
+                int value=DUMMYVAL;
+                int TempValue=DUMMYVAL;
+                int BestDomino=0;
+                int LOR=0;
+                for (int i = 0; i < stop; i++)
+                {
+                    UsingDomino = Hand[0];
+                    Hand.RemoveAt(0);
+                    if(CanIMove(ActualGame,UsingDomino,1)){
+                        ActualGame.AddLast(SetPositionOfDomino(ActualGame,UsingDomino,1));
+                        TempValue = GetBestMove(ActualGame, 0, RecursiveCalls, idPlayer + 1);
+                        ActualGame.RemoveLast();
+                        Console.WriteLine("En la derecha La ficha " + UsingDomino.Item1 + "|" + UsingDomino.Item2 + " Tiene  " + TempValue);
+                    }
+                    if ((value) > (TempValue))
+                    {
+                        value = TempValue;
+                        BestDomino = i;
+                        LOR = 1;
+                    }
+                     if(CanIMove(ActualGame,UsingDomino,-1)){
+                        ActualGame.AddFirst(SetPositionOfDomino(ActualGame,UsingDomino,-1));
+                        TempValue = GetBestMove(ActualGame, 0, RecursiveCalls, idPlayer + 1);
+                        ActualGame.RemoveFirst();
+                        Console.WriteLine("En la izquierda La ficha " + UsingDomino.Item1 + "|" + UsingDomino.Item2 + " Tiene  " + TempValue);
+                    
+                    }
+                    if ((value) > (TempValue))
+                    {
+                        value = TempValue;
+                        BestDomino = i;
+                        LOR = -1;
+                    }
+                   Hand.Add(UsingDomino);
+                }
+                char side;
+                if (LOR < 0) side = 'L';
+                else side = 'R';
+               UsingDomino= MakeAMove(ActualGame, BestDomino + 1, side);
+               Console.Read();
+            }
+
+            EnemysHandCount[idPlayer]--;
+            return UsingDomino;
+        }
+
+        
+        private string GetStringIndex(LinkedList<Tuple<int, int>> ActualGame)
+        {
+            string idx;
+            idx = PrintHand(true);
+
+            string map = "";
+            LinkedListNode<Tuple<int, int>> Node = ActualGame.First;
+            for (int i = 0; i < ActualGame.Count; i++)
+            {
+                if (Node.Value.Item1 == Node.Value.Item2)
+                {
+                    Node = Node.Next;
+                    continue;
+                }
+
+                map += Node.Value.Item1.ToString() + Node.Value.Item1.ToString();
+                Node = Node.Next;
+            }
+            return map + idx;
+        }
+        private int GetBestMove(LinkedList<Tuple<int, int>> ActualGame, int depth, int finalDepth, int IsMe)
+        {
+            string idx = GetStringIndex(ActualGame);
+            if (dpHashTable.Contains(idx) == true) return (int)dpHashTable[idx];
+
+            int value = Eval(ActualGame);
+            if (finalDepth == 0 || value == 1)
+            { 
+                dpHashTable.Add(idx, value); 
+                return value; 
+            }
+
+            if (DUMMYVAL == value) {
+                if (dpHashTable.Contains(idx) == false) dpHashTable.Add(idx, value);
+                return value; }
+
+            Tuple<int,int>UsingDomino;
+            int tempValue = DUMMYVAL;
+            IsMe %= 4;
+            int stop;
+            if (IsMe != idPlayer) stop = AvailableDominoes.Count;
+            else stop = Hand.Count;
+            for (int i = 0; i < stop; i++)
+            {
+                if (IsMe == idPlayer)
+                {
+                    UsingDomino = Hand[0];
+                    Hand.RemoveAt(0);
+                }
+                else
+                {
+                    UsingDomino = AvailableDominoes[0];
+                    AvailableDominoes.RemoveAt(0);
+                }
+                if (CanIMove(ActualGame,UsingDomino,1))
+                {
+                    EnemysHandCount[IsMe]--;
+                    ActualGame.AddLast(SetPositionOfDomino(ActualGame,UsingDomino,1));
+                    tempValue = GetBestMove(ActualGame, depth + 1, finalDepth - 1,IsMe+1);
+                    ActualGame.RemoveLast();
+
+                    EnemysHandCount[IsMe]++;
+                    if ((value) > (tempValue)) { value = (tempValue); }
+                }
+                else if (CanIMove(ActualGame, UsingDomino, -1))
+                {
+                    EnemysHandCount[IsMe]--;
+                    ActualGame.AddFirst(SetPositionOfDomino(ActualGame, UsingDomino, -1));
+                    tempValue = GetBestMove(ActualGame, depth + 1, finalDepth - 1, IsMe + 1);
+                    ActualGame.RemoveFirst();
+
+                    EnemysHandCount[IsMe]++;
+                    if ((value) > (tempValue)) { value = (tempValue); }
+                }
+
+                if (IsMe == idPlayer)
+                    Hand.Add(UsingDomino);
+                else
+                    AvailableDominoes.Add(UsingDomino);
+            }
+            if(!dpHashTable.Contains(idx)) dpHashTable.Add(idx, value);
+            return value;
+        }
+        
+        public Tuple<int, int> DummyMove(LinkedList<Tuple<int, int>> ActualGame)
+        {
+            Tuple<int, int> res =null;
+            if (ActualGame.Count == 0)
+            {
+                Random rnd = new Random();
+                int id= rnd.Next(0, Hand.Count);
+                res = Hand[id];
+                Hand.RemoveAt(id);
+                ActualGame.AddFirst(res);
+                return res;
+            }
+            for (int i = 0; i < Hand.Count; i++)
+            {
+                if (Hand[i].Item1 == ActualGame.First.Value.Item1)
+                {
+                    res= new Tuple<int, int>(Hand[i].Item2, Hand[i].Item1);
+                    ActualGame.AddFirst(res);
+                    Hand.RemoveAt(i);
+                }
+                else if (Hand[i].Item2 == ActualGame.First.Value.Item1)
+                {
+                    res = new Tuple<int, int>(Hand[i].Item1, Hand[i].Item2);
+                    ActualGame.AddFirst(res);
+                    Hand.RemoveAt(i);
+                }
+                else if (Hand[i].Item1 == ActualGame.Last.Value.Item2)
+                {
+                    res = new Tuple<int, int>(Hand[i].Item1, Hand[i].Item2);
+                    ActualGame.AddLast(res);
+                    Hand.RemoveAt(i);
+                }
+                else if (Hand[i].Item2 == ActualGame.Last.Value.Item2)
+                {
+                    res = new Tuple<int, int>(Hand[i].Item2, Hand[i].Item1);
+                    ActualGame.AddLast(res);
+                    Hand.RemoveAt(i);
+                }
+                if (res != null) break;
+            }
+            return res;
+        }
+
+        private Tuple<int, int> SetPositionOfDomino(LinkedList<Tuple<int, int>> ActualGame, Tuple<int, int> UsingDomino, int side)
+        {
+            if (side == 1)
+            {
+                if (ActualGame.Last.Value.Item2 == UsingDomino.Item1)
+                {
+                    return UsingDomino;
+                }
+                if (ActualGame.Last.Value.Item2 == UsingDomino.Item2)
+                {
+                    return new Tuple<int, int>(UsingDomino.Item2, UsingDomino.Item1);
+                }
+            }
+            else
+            {
+                if (ActualGame.First.Value.Item1 == UsingDomino.Item1)
+                {
+                    return new Tuple<int, int>(UsingDomino.Item2, UsingDomino.Item1);
+                }
+                if (ActualGame.First.Value.Item1 == UsingDomino.Item2)
+                    return UsingDomino;
+            }
+            return null;
+        }
+
+        private bool CanIMove(LinkedList<Tuple<int, int>> ActualGame, Tuple<int, int> UsingDomino, int side)
+        {
+            //if (ActualGame.Count == 0)
+            {
+               // DummyMove(ActualGame);
+             //   return true;
+            }
+            if (side == 1)
+            {
+                if (ActualGame.Last.Value.Item2 == UsingDomino.Item1 || ActualGame.Last.Value.Item2 == UsingDomino.Item2)
+                    return true;
+            }
+            else
+            {
+                if (ActualGame.First.Value.Item1 == UsingDomino.Item1 || ActualGame.First.Value.Item1 == UsingDomino.Item2)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Dominoes/DominoBoard.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Diagnostics;
+using System.Threading;
+namespace Dominoes
+{
+    class DominoBoard
+    {
+
+        Bot P1;
+        Bot P2, P3, P4;
+        List<string> TimesP = new List<string>();
+        LinkedList<Tuple<int, int>> DominoGame;
+
+        public DominoBoard()
+        {
+            DominoGame = new LinkedList<Tuple<int, int>>();
+
+            List<Tuple<int, int>> L = new List<Tuple<int, int>>();
+            List<Tuple<int, int>> Q = new List<Tuple<int, int>>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = i; j < 7; j++)
+                {
+                    Q.Add(new Tuple<int,int>(i,j));
+                }
+            }
+            Shuffle(Q);
+            for (int i = 0; i < 7; i++)
+            {
+                L.Add(Q[i]);
+            }
+            P1 = new Bot(L.ToArray().ToList(),1,0);
+            L.Clear();
+            //L.Add(new Tuple<int,int>(
+            for (int i = 7; i < 14; i++)
+            {
+                L.Add(Q[i]);
+            }
+            P2 = new Bot(L.ToArray().ToList(), -1,1);
+            L.Clear();
+            for (int i = 14; i < 21; i++)
+            {
+                L.Add(Q[i]);
+            }
+            P3 = new Bot(L.ToArray().ToList(), -1,2);
+            L.Clear();
+            for (int i = 21; i < 28; i++)
+            {
+                L.Add(Q[i]);
+            }
+            P4 = new Bot(L.ToArray().ToList(), -1,3);
+            L.Clear();
+            Q.Clear();
+            GC.Collect();
+        }
+
+        private Random _random = new Random();
+        /// <summary>
+        /// Shuffle the array.
+        /// </summary>
+        /// <typeparam name="T">Array element type.</typeparam>
+        /// <param name="array">Array to shuffle.</param>
+        public void Shuffle(List<Tuple<int,int>> array)
+        {
+
+            var random = _random;
+            for (int i = array.Count; i > 1; i--)
+            {
+                // Pick random element to swap.
+                int j = random.Next(i); // 0 <= j <= i-1
+                // Swap.
+                Tuple<int, int> tmp = array[j];
+                array[j] = array[i - 1];
+                array[i - 1] = tmp;
+            }
+        }
+
+        public void PrintGame()
+        {
+            LinkedListNode<Tuple<int, int>> Nodes = DominoGame.First;
+
+            Console.WriteLine("\n\n========================================================================");
+            Console.Write(" ");
+            for (int i = 0; i < DominoGame.Count; i++)
+            {
+                Console.Write(Nodes.Value.Item1.ToString() + '|' + Nodes.Value.Item2.ToString() + " ");
+                Nodes = Nodes.Next;
+            }
+            Console.WriteLine();
+            Console.WriteLine("========================================================================\n\n");
+           //Console.WriteLine(P1.PrintHand());
+           //Console.WriteLine(P2.PrintHand());
+           //Console.WriteLine(P3.PrintHand());
+           //Console.WriteLine(P4.PrintHand());
+        }
+      
+        Tuple<int, int> PlayerPlaying(Player P)
+        {
+            bool canPlay = P.CanPlay(DominoGame);
+            if (!canPlay)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine(P.PrintHand());
+                Console.WriteLine("-----------------------------\n No vas...");
+                Thread.Sleep(1500);
+                return null;
+            }
+
+            Tuple<int, int> res = null;
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine(P.PrintHand());
+            while (res == null)
+            {
+                Console.WriteLine("-----------------------------\n Digita el lado y el id de la ficha.");
+                char side = Convert.ToChar(Console.ReadLine());
+                int id = Convert.ToInt32(Console.ReadLine());
+                res =P.MakeAMove(DominoGame, id, (char)side);
+            }
+            return res;//P.MakeAMove(DominoGame, id, char.Parse(side.ToString()));
+        }
+
+        Tuple<int, int> PlayerPlaying(Bot B)
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(B.PrintHand());
+            Console.WriteLine("-----------------------------\n");
+            Tuple<int, int> res;
+            // @TODO Eliminar el dummyMove
+            if (B.idPlayer == 0)
+                res = B.DummyMove(DominoGame);
+           else
+                res=B.MakeAMove(DominoGame);
+
+            if (res == null) Console.WriteLine("El jugador " + (B.idPlayer+1).ToString() + " No va waching...\n");
+            return res;
+        }
+
+        public int DoblesCant( Player Pl)
+        {
+            int dbs = 0;
+            for (int i = 0; i < P1.Hand.Count; i++)
+                if (Pl.Hand[i].Item1 == Pl.Hand[i].Item2) dbs++;
+            return dbs;
+        }
+
+        public void PrintHands()
+        {
+            Console.WriteLine("P1:  " + P1.PrintHand());
+            Console.WriteLine("P2:  " + P2.PrintHand());
+            Console.WriteLine("P3:  " + P3.PrintHand());
+            Console.WriteLine("P4:  " + P4.PrintHand());
+        }
+
+        public void PlayDomino()
+        {
+            //if (DoblesCant(P1) >= 3 || DoblesCant(P2) >= 3 || DoblesCant(P3) >= 3 || DoblesCant(P4) >= 3)
+            //{
+            //    Console.WriteLine("Hay un jugador con 3 dobles o mas");
+            //  //  return;
+            //}
+            int EndGame = 0;
+            Stopwatch clock = Stopwatch.StartNew();
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 4)
+                {
+                    i = 0;
+                }
+                if (EndGame == 4 || P1.Hand.Count == 0 || P2.Hand.Count == 0 || P3.Hand.Count == 0 || P4.Hand.Count == 0)
+                {
+                    Console.WriteLine("||||||||||| Juego terminado|||||||||||");
+                    PrintGame();
+                    PrintHands();
+                    for (int q = 0; q < TimesP.Count;q++) Console.WriteLine(TimesP[q]);
+                    Console.WriteLine("Con un aproximado de:" + " P2" + P2.dpHashTable.Count + " P3 " + P3.dpHashTable.Count + " P4 " + P4.dpHashTable.Count+" Nodos");
+                    Console.Read();
+                    return;
+                }
+                Console.Clear();
+                string t = clock.Elapsed.ToString();
+                Console.WriteLine(t);
+                TimesP.Add("P"+(i+1)+": "+t);
+                Console.WriteLine("||||||||||| Esta jugando el jugador " + (i + 1).ToString() + " |||||||||||");
+                PrintGame();
+
+                PrintHands();
+                Tuple<int, int> res=null;
+                Console.Read();
+                if (i == 0)
+                {
+                    res= PlayerPlaying(P1);
+                    if ( res== null) EndGame++;
+                    else EndGame = 0;
+                }
+                else if (i == 1)
+                {
+                    res = PlayerPlaying(P2);
+                    if (res == null) EndGame++;
+                    else EndGame = 0;
+                }
+                else if (i == 2)
+                {
+                    res = PlayerPlaying(P3);
+                    if (res == null) EndGame++;
+                    else EndGame = 0;
+                }
+                else//if (i==3)
+                {
+                    res = PlayerPlaying(P4);
+                    if (res == null) EndGame++;
+                    else EndGame = 0;
+                }
+                PlayersLearn(i, res);
+                //Console.Read();
+   //             Thread.Sleep(1000);
+            }
+        }
+
+        public void PlayersLearn(int idPlayer,Tuple<int, int> Moved)
+        {
+            if (idPlayer != 0)
+                P1.Learn(DominoGame, 0, Moved);
+            if (idPlayer != 1)
+                P2.Learn(DominoGame, 1, Moved);
+            if (idPlayer != 2)
+                P3.Learn(DominoGame, 2, Moved);
+            if (idPlayer != 3)
+                P4.Learn(DominoGame, 3, Moved);
+        }
+
+    }
+}
